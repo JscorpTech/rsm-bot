eeff19aa6302348f30fba018a852389d40b4d23d
A'zamov Samandar <admin@jscorp.uz>
2025-08-20 23:08:51 +0500

    fix typo

--- config/conf/navigation.py
+++ config/conf/navigation.py
@@ -19,12 +19,74 @@ PAGES = [
             {
                 "title": _("Users"),
                 "icon": "group",
-                "link": reverse_lazy("admin:http_user_changelist"),
+                "link": reverse_lazy("admin:accounts_user_changelist"),
             },
             {
                 "title": _("Group"),
                 "icon": "group",
                 "link": reverse_lazy("admin:auth_group_changelist"),
+            },
+        ],
+    },
+    {
+        "title": _("Dashboard"),
+        "separator": True,  # Top border
+        "items": [
+            {
+                "title": _("Manzillar"),
+                "icon": "add_location",
+                "link": reverse_lazy("admin:bot_addressmodel_changelist"),
+            },
+            {
+                "title": _("Category"),
+                "icon": "category",
+                "link": reverse_lazy("admin:bot_categorymodel_changelist"),
+            },
+            {
+                "title": _("Paketlar"),
+                "icon": "package_2",
+                "link": reverse_lazy("admin:bot_packagemodel_changelist"),
+            },
+            {
+                "title": _("Bot foydalanuvchilari"),
+                "icon": "groups",
+                "link": reverse_lazy("admin:bot_botuser_changelist"),
+            },
+            {
+                "title": _("Tarjimalar"),
+                "icon": "translate",
+                "link": reverse_lazy("admin:bot_messages_changelist"),
+            },
+            {
+                "title": _("Mexmonxonalar"),
+                "icon": "hotel",
+                "link": reverse_lazy("admin:bot_hotelmodel_changelist"),
+            },
+        ],
+    },
+    {
+        "title": _("Buyurtmalar"),
+        "separator": True,  # Top border
+        "items": [
+            {
+                "title": _("Mexmonxona buyurtmalar"),
+                "icon": "hotel",
+                "link": reverse_lazy("admin:bot_hotelorder_changelist"),
+            },
+            {
+                "title": _("visa buyurtmalar"),
+                "icon": "card_membership",
+                "link": reverse_lazy("admin:bot_visaorder_changelist"),
+            },
+            {
+                "title": _("Transfer buyurtmalar"),
+                "icon": "airport_shuttle",
+                "link": reverse_lazy("admin:bot_transferorder_changelist"),
+            },
+            {
+                "title": _("Mini Tour buyurtmalar"),
+                "icon": "festival",
+                "link": reverse_lazy("admin:bot_minitourorder_changelist"),
             },
         ],
     },
